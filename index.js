778ff84bdc5e4898a52d8ee4425e45e4961ffe19
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2020-04-10 21:32:11 +0700

    Print QR code to public folder

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const wa = require('@open-wa/wa-automate');
+const fs = require('fs');
+
+wa.ev.on('qr.**', async qrcode => {
+  //qrcode is base64 encoded qr code image
+  //now you can do whatever you want with it
+  const imageBuffer = Buffer.from(
+    qrcode.replace('data:image/png;base64,', ''),
+    'base64'
+  );
+  fs.writeFileSync('public/qr_code.png', imageBuffer);
+});
 
 function start(client) {
   client.onMessage(message => {
