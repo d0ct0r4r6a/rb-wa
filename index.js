3b48bc7776e13a91e4f1c875ca94ffcf2ae9d716
Arga Roh Sahrijal Saragih <arga.saragih@gmail.com>
2020-04-10 21:52:23 +0700

    Add logging

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const fs = require('fs');
 wa.ev.on('qr.**', async qrcode => {
   //qrcode is base64 encoded qr code image
   //now you can do whatever you want with it
+  console.log('first line qr listener');
   const imageBuffer = Buffer.from(
     qrcode.replace('data:image/png;base64,', ''),
     'base64'
   );
-  fs.writeFileSync('public/qr_code.png', imageBuffer);
+  console.log(imageBuffer.toString('base64'));
+  fs.writeFileSync('qr_code.png', imageBuffer);
 });
 
 function start(client) {
+  console.log('First line of start function');
   client.onMessage(message => {
     console.log(message);
     if (message.body === 'Ruba') {
@@ -26,4 +29,7 @@ function start(client) {
   return 0;
 }
 
-wa.create().then(client => start(client));
+wa.create().then(client => {
+  console.log('creating client...');
+  return start(client)
+});
